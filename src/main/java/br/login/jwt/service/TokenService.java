6801dd90bd5b9f64f4f806a0aaab3e22e1608fd1
Rafael Aguiar <rafael.aguiar.im@gmail.com>
2023-02-13 23:25:50 -0300

    Implementação do método para capturar o token gerado e método para  para verificar se o token é válido

--- src/main/java/br/login/jwt/service/TokenService.java
+++ src/main/java/br/login/jwt/service/TokenService.java
@@ -4,6 +4,7 @@ import com.auth0.jwt.JWT;
 import com.auth0.jwt.algorithms.Algorithm;
 import com.auth0.jwt.exceptions.JWTCreationException;
 import br.login.jwt.domain.Usuario;
+import com.auth0.jwt.exceptions.JWTVerificationException;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
@@ -14,25 +15,39 @@ import java.time.ZoneOffset;
 @Service
 public class TokenService {
 
-    //caminho criado no aplication.property para acessar a variável de ambiente
     @Value("${api.security.token.secret}")
     private String secret;
 
-    //Codigo da Api JWT
+    //Codigo da Api JWT para gerar o token JWT
     public String gerarToken(Usuario usuario) {
         try {
             var algorithm = Algorithm.HMAC256(secret);
             return JWT.create()
-                    .withIssuer("Login Spring security - JWT")
+                    .withIssuer("API Voll.med")
                     .withSubject(usuario.getLogin())
                     .withExpiresAt(dataExpiracao())
                     .sign(algorithm);
         } catch (JWTCreationException exception){
-            throw new RuntimeException("Erro ao gerrar token jwt", exception);
+            throw new RuntimeException("Erro ao gerar token jwt", exception);
         }
 
     }
+
+    //Codigo da Api JWT para validar o token JWT
+    public String getSubject(String tokenJWT){
+        try {
+            var algorithm = Algorithm.HMAC256(secret);
+            return JWT.require(algorithm)
+                    .withIssuer("API Voll.med")
+                    .build()
+                    .verify(tokenJWT)
+                    .getSubject();
+        } catch (JWTVerificationException exception) {
+            throw new RuntimeException("Token JWT inválido ou expirado!");
+        }
+    }
+
     private Instant dataExpiracao() {
-        return LocalDateTime.now().plusHours(2).toInstant(ZoneOffset.of("-03:00"));
+        return LocalDateTime.now().plusHours(1).toInstant(ZoneOffset.of("-03:00"));
     }
 }
---
+++ src/main/java/br/login/jwt/utils/security/SecurityFilter.java
@@ -0,0 +1,40 @@
+package br.login.jwt.utils.security;
+
+import br.login.jwt.service.TokenService;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+@Component //Identifica que é uma classe genérica
+public class SecurityFilter extends OncePerRequestFilter {
+
+    @Autowired
+    private TokenService tokenService;
+
+    //Garante que essa classe será executada apenas uma unica vez a cada requisição
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        var tokenJWT = recuperarToken(request);
+
+        //chamada do método que valida o token JWT
+       var subject = tokenService.getSubject(tokenJWT);
+
+        filterChain.doFilter(request,response);
+
+    }
+    //Método para recuperar o token enviado ao cabeçalho Authorization
+    private String recuperarToken(HttpServletRequest request){
+        var authorizationHeader = request.getHeader("Authorization");
+        if (authorizationHeader == null){
+            throw new RuntimeException("Token JWT não foi enviado ao cabeçalho Authorization!");
+        }
+
+        return authorizationHeader.replace("Bearer ", "");
+    }
+}
