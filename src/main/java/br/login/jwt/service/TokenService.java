9189b9582495eecd74f02983dd21df6d15cd3160
Rafael Aguiar <rafael.aguiar.im@gmail.com>
2023-02-14 00:47:29 -0300

    Implementação do filtro que verifica se o usuario esta logado ou não

--- src/main/java/br/login/jwt/service/TokenService.java
+++ src/main/java/br/login/jwt/service/TokenService.java
@@ -23,7 +23,7 @@ public class TokenService {
         try {
             var algorithm = Algorithm.HMAC256(secret);
             return JWT.create()
-                    .withIssuer("API Voll.med")
+                    .withIssuer("Spring Security JWT")
                     .withSubject(usuario.getLogin())
                     .withExpiresAt(dataExpiracao())
                     .sign(algorithm);
@@ -38,7 +38,7 @@ public class TokenService {
         try {
             var algorithm = Algorithm.HMAC256(secret);
             return JWT.require(algorithm)
-                    .withIssuer("API Voll.med")
+                    .withIssuer("Spring Security JWT")
                     .build()
                     .verify(tokenJWT)
                     .getSubject();
--- src/main/java/br/login/jwt/utils/security/SecurityConfiguration.java
+++ src/main/java/br/login/jwt/utils/security/SecurityConfiguration.java
@@ -1,7 +1,9 @@
 package br.login.jwt.utils.security;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -10,17 +12,26 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfiguration {
+
+    @Autowired
+    private  SecurityFilter securityFilter;
+
     //O @Bean serve apra exportar uma classe para o Spring, fazendo com que ele consiga carregála e realize
     //a sua injeção de dependencia em outras classes
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception{
         return http.csrf().disable()//Desabilitaa a proteção contra ataques CSRF, pos o token já possui
                 .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
-                .and().build();
+                .and().authorizeHttpRequests()
+                .requestMatchers(HttpMethod.POST, "/login").permitAll() //Permite todas as tentatativas de login
+                .anyRequest().authenticated() // as demais requisições, somente se estiver logado
+                .and().addFilterBefore(securityFilter, UsernamePasswordAuthenticationFilter.class)//Definição de qual filtro chamar primeiro
+                .build();
     }
 
     @Bean
